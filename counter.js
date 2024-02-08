61b387ca674b16d99c558faebb6fd19568c1a390
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-02-08 08:18:06 -0800

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,3 +1,2 @@
 
-<script type="text/javascript" src="//widget.supercounters.com/ssl/online_t.js"></script><script type="text/javascript">sc_online_t(1685019,"Users Online","ffffff");</script><br><noscript><a href="https://www.supercounters.com/">supercounters.com</a></noscript>
-
+text/javascript" src="//widget.supercounters.com/ssl/online_t.js" text/javascript">sc_online_t(1685019,"Users Online","ffffff");
